ecdf6e088867a0e2dc4a82665c72c2f02c55d5e7
suika343 <64908070+suika343@users.noreply.github.com>
2025-03-27 02:12:39 +0800

    after lesson 8.6
    GameManager script (skeleton) constructed

---
+++ DungeonGunner/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+
+    #region HEADER DUNGEON LEVELS
+    [Space(10)]
+    [Header("Dungeon Levels")]
+    #endregion
+
+    #region Tooltip
+    [Tooltip("List of all the dungeon levels in the game, populate with Dungeon Level Scriptable Objects")]
+    #endregion
+    [SerializeField]
+    private List<DungeonLevelSO> dungeonLevelList;
+
+    #region Tooltip
+    [Tooltip("Populate with starting level for testing, first level = 0")]
+    #endregion
+    [SerializeField]
+    private int currentDungeonLevelListIndex = 0;
+
+    [SerializeField]
+    private GameState gameState;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        gameState = GameState.gameStarted;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleGameState();
+
+        //testing
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameState = GameState.gameStarted;
+        }
+    }
+
+    private void HandleGameState()
+    {
+        switch(gameState)
+        {
+            case GameState.gameStarted:
+                Debug.Log("Game Started");
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+                gameState = GameState.playingLevel;
+                break; 
+        }
+    }
+
+    private void PlayDungeonLevel(int dungeonLevelListIndex)
+    {
+        DungeonLevelSO dungeonLevel = dungeonLevelList[dungeonLevelListIndex];
+        Debug.Log("Playing Dungeon Level: " + dungeonLevel.levelName);
+    }
+
+    #region VALIDATION
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(dungeonLevelList), dungeonLevelList);
+    }
+#endif
+    #endregion
+}
