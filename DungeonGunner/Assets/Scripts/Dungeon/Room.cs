1f6a195f4661d9185468d8777ee4c81a13fa0ad2
suika343 <64908070+suika343@users.noreply.github.com>
2025-03-30 04:16:08 +0800

    after lesson 8.7
    Shader Graph
    -Sprite-Lit URP Shader

---
+++ DungeonGunner/Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room 
+{
+    public string id;
+    public string templateID;
+    public GameObject prefab;
+    public RoomNodeTypeSO roomNodeType;
+    //bounds when the room is placed
+    public Vector2Int lowerBounds;
+    public Vector2Int upperBounds;
+    //bounds in the room template
+    public Vector2Int templateLowerBounds;
+    public Vector2Int templateUpperBounds;
+
+    public Vector2Int[] spawnPointsArray;
+    public List<string> childRoomIDList;
+    public string parentRoomID;
+    public List<Doorway> doorwayList;
+    public bool isPositioned = false;
+    public InstantiatedRoom instantiatedRoom;
+
+    public bool isLit = false;
+    public bool isClearOfEnemies = false;
+    public bool isPreviouslyVisited = false;
+
+    public Room()
+    {
+        childRoomIDList = new List<string>();
+        doorwayList = new List<Doorway>();
+    }
+}
