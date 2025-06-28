3cfb735d9e1596d7d29451a4b2b8c2991b2cf33b
suika343 <64908070+suika343@users.noreply.github.com>
2025-06-28 23:34:51 +0800

    9.6 Cinemachine Target Group
    Add cursor to cinemachine target group

--- DungeonGunner/Assets/Scripts/Misc/CinemachineTarget.cs
+++ DungeonGunner/Assets/Scripts/Misc/CinemachineTarget.cs
@@ -5,6 +5,8 @@ using Unity.Cinemachine;
 public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
+    [Tooltip("Populate with the CursorTarget GameObject")]
+    [SerializeField] private Transform cursorTarget;
 
     private void Awake()
     {
@@ -16,16 +18,28 @@ public class CinemachineTarget : MonoBehaviour
         SetCinemachineTargetGroup();
     }
 
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
+    }
+
     private void SetCinemachineTargetGroup()
     {
         CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target
         {
             Object = GameManager.Instance.GetPlayer().transform,
             Weight = 1,
+            Radius = 2.5f
+        };
+
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target
+        {
+            Object = cursorTarget,
+            Weight = 1,
             Radius = 1f
         };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player };
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player, cinemachineGroupTarget_cursor };
 
         // Updated to use the 'Targets' property instead of the obsolete 'm_Targets' field
         cinemachineTargetGroup.Targets = new System.Collections.Generic.List<CinemachineTargetGroup.Target>(cinemachineTargetArray);
