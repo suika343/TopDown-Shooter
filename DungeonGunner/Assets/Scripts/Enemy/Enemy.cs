11a92c1865aa738de33753e168545cdb183823c9
suika343 <64908070+suika343@users.noreply.github.com>
2026-03-02 22:56:47 +0800

    17.2 complete
    - Create Enemy Prefab and Scriptable Objects

---
+++ DungeonGunner/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+#endregion
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector] public EnemyDetailsSO enemyDetails;
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+    [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+
+    private void Awake()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
+}
---
+++ DungeonGunner/Assets/Scripts/Enemy/EnemyDetailsSO.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDetails_", menuName = "Scriptable Objects/Enemy/Enemy Details")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    #region HEADER
+    [Space(10)]
+    [Header("BASE ENEMY DETAILS")]
+    #endregion
+
+    public string enemyName;
+
+    public GameObject enemyPrefab;
+
+    #region VALIDATION
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(enemyName), enemyName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(enemyPrefab), enemyPrefab);
+    }
+#endif
+    #endregion
+}
