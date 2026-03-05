104b590b897ba856671a42b9426c71d6b2f3f223
suika343 <64908070+suika343@users.noreply.github.com>
2026-03-05 22:19:55 +0800

    18 completed
    - set up animation controllers to enemies and bosses

--- DungeonGunner/Assets/Scripts/Enemy/Enemy.cs
+++ DungeonGunner/Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine.Rendering;
 #region REQUIRE COMPONENTS
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(PolygonCollider2D))]
@@ -15,11 +16,13 @@ public class Enemy : MonoBehaviour
     private CircleCollider2D circleCollider2D;
     private PolygonCollider2D polygonCollider2D;
     [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+    [HideInInspector] public Animator animator;
 
     private void Awake()
     {
         circleCollider2D = GetComponent<CircleCollider2D>();
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 }
