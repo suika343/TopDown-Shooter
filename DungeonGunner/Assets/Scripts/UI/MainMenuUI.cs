20599572f1d26d52a8cc6a2695f5331e2d315ab7
suika343 <64908070+suika343@users.noreply.github.com>
2026-07-20 01:49:04 +0800

    32.4 Completed
    - Implement instructions scene

--- DungeonGunner/Assets/Scripts/UI/MainMenuUI.cs
+++ DungeonGunner/Assets/Scripts/UI/MainMenuUI.cs
@@ -11,8 +11,11 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject playButton;
     [SerializeField] private GameObject highScoreButton;
     [SerializeField] private GameObject returnToMainMenuButton;
+    [SerializeField] private GameObject quitButton;
+    [SerializeField] private GameObject instructionsButton;
 
     private bool isHighScoresSceneLoaded = false;
+    private bool isInstructionsSceneLoaded = false;
     void Start()
     {
         SceneManager.LoadScene("CharacterSelectorScene", LoadSceneMode.Additive);
@@ -30,6 +33,8 @@ public class MainMenuUI : MonoBehaviour
     public void LoadHighScores()
     {
         playButton.SetActive(false);
+        quitButton.SetActive(false);
+        instructionsButton.SetActive(false);
         highScoreButton.SetActive(false);
 
         isHighScoresSceneLoaded = true;
@@ -40,22 +45,48 @@ public class MainMenuUI : MonoBehaviour
         SceneManager.LoadScene("HighScoreScene", LoadSceneMode.Additive);
     }
 
+    public void LoadInstructions()
+    {
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        instructionsButton.SetActive(false);
+        highScoreButton.SetActive(false);
+
+        isInstructionsSceneLoaded = true;
+
+        SceneManager.UnloadSceneAsync("CharacterSelectorScene");
+        returnToMainMenuButton.SetActive(true);
+
+        SceneManager.LoadScene("InstructionsScene", LoadSceneMode.Additive);
+    }
+
     public void LoadCharacterSelector() {
 
         returnToMainMenuButton.SetActive(false);
-
+        
         if (isHighScoresSceneLoaded)
         {
             SceneManager.UnloadSceneAsync("HighScoreScene");
             isHighScoresSceneLoaded = false;
-
+        }
+        else if (isInstructionsSceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync("InstructionsScene");
+            isInstructionsSceneLoaded = false;
         }
 
         highScoreButton.SetActive(true);
         playButton.SetActive(true);
+        instructionsButton.SetActive(true);
+        quitButton.SetActive(true);
 
         SceneManager.LoadScene("CharacterSelectorScene", LoadSceneMode.Additive);
 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     #region VALIDATION
@@ -65,6 +96,8 @@ public class MainMenuUI : MonoBehaviour
         HelperUtilities.ValidateCheckNullValue(this, nameof(playButton), playButton);
         HelperUtilities.ValidateCheckNullValue(this, nameof(highScoreButton), highScoreButton);
         HelperUtilities.ValidateCheckNullValue(this, nameof(returnToMainMenuButton), returnToMainMenuButton);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(quitButton), quitButton);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(instructionsButton), instructionsButton);
     }
 #endif
     #endregion
